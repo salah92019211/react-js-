a364f6ab0d7b1ad1c6c907e02d53d0893a90cc2c
salah92019211 <bouazizsalah171@gmail.com>
2023-04-08 07:45:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Oneoneone from './Oneoneone'
+import Bodycards from './Bodycards'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Threthre from './Threthre'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <div><Oneoneone></Oneoneone></div>
+     <div><Threthre></Threthre></div>
+     <div><Bodycards></Bodycards> </div>
+      
     </div>
   );
 }
